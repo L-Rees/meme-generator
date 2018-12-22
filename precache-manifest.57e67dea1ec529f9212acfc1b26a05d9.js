4b9bf50945538a7b1b312bef910df1fda29fc1bf
Lisa Rees <kortirion@gmail.com>
2018-12-22 18:12:19 -0500

    Updates

---
+++ precache-manifest.57e67dea1ec529f9212acfc1b26a05d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ce2fa357a217b85a639",
+    "url": "/meme-generator/static/css/main.76fac63d.chunk.css"
+  },
+  {
+    "revision": "6ce2fa357a217b85a639",
+    "url": "/meme-generator/static/js/main.6ce2fa35.chunk.js"
+  },
+  {
+    "revision": "4cbbb6fee154183d4dbf",
+    "url": "/meme-generator/static/js/1.4cbbb6fe.chunk.js"
+  },
+  {
+    "revision": "340a140dc48e109e29a4",
+    "url": "/meme-generator/static/js/runtime~main.340a140d.js"
+  },
+  {
+    "revision": "7f28528a8f098808ddb4152745b4a8f1",
+    "url": "/meme-generator/index.html"
+  }
+];
