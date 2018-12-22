0e4ff056f32ff6618afe0a38e498b9626a8cc917
Lisa Rees <kortirion@gmail.com>
2018-12-21 23:20:03 -0500

    styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -40,7 +40,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Meme Generator</h1>
+        <header>
+          <h1>Meme Generator</h1>
+        </header>
      
         <MemeGenerator
           image = {this.state.image}
